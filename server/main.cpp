0a3dae93ede3e78e9564bc6d96304460c08d6b76
Andrei G <oz@oprod.net>
2012-04-14 18:17:56 +0300

    oops. return 0 ;)

--- server/main.cpp
+++ server/main.cpp
@@ -10,6 +10,7 @@ using namespace::std;
 int main(int argc, char **argv) {
    if (argc < 2) {
       cout << "Usage "<< argv[0] <<" port";
+      return 0;
    }
    int port = atoi(argv[1]);
    return 0;
