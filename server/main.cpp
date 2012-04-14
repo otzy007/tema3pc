590cb29d2bab7200fce25e21307534651cabc11d
Andrei G <oz@oprod.net>
2012-04-14 18:16:28 +0300

    verificare port la server

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 using namespace::std;
 int main(int argc, char **argv) {
-
-    return 0;
+   if (argc < 2) {
+      cout << "Usage "<< argv[0] <<" port";
+   }
+   int port = atoi(argv[1]);
+   return 0;
 }
