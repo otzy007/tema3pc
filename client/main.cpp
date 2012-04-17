57fcd4cbf8b1735ee7df1845e62f01cb7baa9470
Andrei G <oz@oprod.net>
2012-04-17 16:51:43 +0300

    cere si primeste lista de clienti

--- client/main.cpp
+++ client/main.cpp
@@ -6,8 +6,9 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define BUFLEN 256
+#define BUFFLEN 256
 
 using namespace::std;
 int main(int argc, char **argv) {
@@ -23,7 +24,7 @@ int main(int argc, char **argv) {
    fd_set read_fds;
    fd_set tmp_fds;
    
-   char buffer[BUFLEN];
+   char buffer[BUFFLEN];
    
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
    
@@ -38,7 +39,7 @@ int main(int argc, char **argv) {
       cout << "ERROR connecting\n";
    
    /* trimite numele de utilizator */
-   bzero(buffer, BUFLEN);
+   bzero(buffer, BUFFLEN);
    buffer[0] = 1;
    strcpy(buffer + 1, argv[1]);
    int n = send(sockfd, buffer, strlen(buffer), 0);
@@ -54,10 +55,48 @@ int main(int argc, char **argv) {
        */
       cout << "Name: " << argv[1] << " already used. Please change it!\n";
       return 0;
-   } else if (buffer[0] == 200) {
+   } else{ if (buffer[0] == 20) {
       /* Totul e OK */
+      cout <<"OKKK\n";
+      FD_SET(STDIN_FILENO, &read_fds); // stdin pentru comenzi
+      FD_SET(sockfd, &read_fds);
+      fdmax = sockfd;
       
-   }
-   
+      while(1) {
+	 tmp_fds = read_fds;
+	 if (select(fdmax + 1, &tmp_fds, NULL, NULL, NULL) == -1)
+	    cout << "ERROR in select\n";
+	 
+	 for (int i = 0; i <= fdmax; i++) {
+	    if (FD_ISSET(i, &tmp_fds)) {
+	       if (i == STDIN_FILENO) {
+		  string command;
+		  cin >> command;
+		  if (command == "listclients") {
+		     bzero(buffer, BUFFLEN);
+		     buffer[0] = 2;
+		     strcpy(buffer + 1, "listclients");
+		     send(sockfd, buffer, strlen(buffer), 0);
+		  }
+	       } else if (i == sockfd) {
+		  int n;
+		  bzero(buffer, BUFFLEN);
+		  if ((n = recv(i, buffer, sizeof(buffer), 0)) <= 0) {
+		     if (n == 0) {
+			cout << "Connection with server closed\n";
+			close(sockfd);
+			return 0;
+		     }
+		     else
+			cout << "ERROR in recv\n";
+		  } else {
+		     cout << buffer << endl;
+		  }
+	       }
+	    }
+	 }
+      }
+   }}
+   close(sockfd);
    return 0;
 }
